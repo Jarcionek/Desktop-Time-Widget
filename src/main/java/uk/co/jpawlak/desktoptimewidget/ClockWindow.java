f0664b912a099a725d8062139390b594aa5fac24
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2021-03-25 21:48:36 +0000

    implemented clock

--- src/main/java/uk/co/jpawlak/desktoptimewidget/ClockWindow.java
+++ src/main/java/uk/co/jpawlak/desktoptimewidget/ClockWindow.java
@@ -2,27 +2,32 @@ package uk.co.jpawlak.desktoptimewidget;
 
 import javax.swing.JLabel;
 import javax.swing.JWindow;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class ClockWindow {
 
-    private static final ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("EEE dd.MM.yyyy HH:mm:ss");
+    private static final ScheduledExecutorService EXECUTOR = Executors.newSingleThreadScheduledExecutor();
 
     private final JWindow window = new JWindow();
     private final JLabel textLabel = new JLabel("");
 
-    private int i = 1;
-
     public ClockWindow() {
         window.setContentPane(textLabel);
 
-        window.setSize(100, 15);
+        window.setSize(130, 15);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
-        service.scheduleAtFixedRate(() -> textLabel.setText("" + i++), 0, 1, TimeUnit.SECONDS);
+        EXECUTOR.scheduleAtFixedRate(this::updateTime, 0, 1, TimeUnit.SECONDS);
+    }
+
+    private void updateTime() {
+        textLabel.setText(DATE_FORMAT.format(new Date()));
     }
 
     public int getWidth() {
